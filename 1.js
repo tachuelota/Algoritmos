284609b12f9bcf74d63d69c6d1c00696e92dfd15
tachuelota <jpbadoino@gmail.com>
2017-06-09 15:41:18 -0500

    Create 1.js

---
+++ 1.js
@@ -0,0 +1,18 @@
+var doSearch = function(array, targetValue) {
+var min = 0;
+var max = array.length - 1;
+var guess;
+
+while(min <= max){
+guess= Math.floor((max + min)/2);
+
+if(array[guess]===targetValue){
+return guess;
+}
+else if(array[guess] < targetValue ) {
+min = guess + 1; 
+}
+else{
+max = guess - 1;
+}
+}
